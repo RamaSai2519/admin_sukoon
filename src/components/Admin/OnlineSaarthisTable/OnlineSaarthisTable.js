fa505533ead16df343adf64aab95747b8e093006
Rama Sathya Sai <rchandik@gitam.in>
2024-05-13 19:10:59 +0530

    chore: Update OnlineSaarthisTable CSS for responsive display and status alignment

--- src/components/Admin/OnlineSaarthisTable/OnlineSaarthisTable.js
+++ src/components/Admin/OnlineSaarthisTable/OnlineSaarthisTable.js
@@ -1,20 +1,31 @@
-// OnlineSaarthisTable.js
 import React from 'react';
 import './OnlineSaarthisTable.css';
 import PersonIcon from '@mui/icons-material/Person';
 import PersonOffIcon from '@mui/icons-material/PersonOff';
 import { red, green } from '@mui/material/colors';
 
+// OnlineSaarthisTable.js
+
 const renderStatusIcon = (saarthi) => {
   console.log(saarthi);
-  if (saarthi.isBusy) {
-    return <PersonOffIcon sx={{ color: red[500] }} />;
-  } else {
+  if (saarthi.isBusy === false) {
     return <PersonIcon sx={{ color: green[500] }} />;
+  } else {
+    return <PersonOffIcon sx={{ color: red[500] }} />;
   }
 }
 
 const OnlineSaarthisTable = ({ onlineSaarthis }) => {
+  // Sort the saarthis array by isBusy === false first
+  const sortedSaarthis = onlineSaarthis.sort((a, b) => {
+    if (a.isBusy === true && b.isBusy === false) {
+      return -1;
+    } else if (a.isBusy === false && b.isBusy === true) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
 
   return (
     <div className="online-table-container">
@@ -22,7 +33,7 @@ const OnlineSaarthisTable = ({ onlineSaarthis }) => {
         <thead>
         </thead>
         <tbody>
-          {onlineSaarthis.map(saarthi => (
+          {sortedSaarthis.map(saarthi => (
             <tr key={saarthi._id}>
               <td>{saarthi.name}</td>
               <td className='status'>{renderStatusIcon(saarthi)}</td>
