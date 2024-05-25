6b4adaedd478f60a926e876c1f6492e43d973a75
Rama Sathya Sai <rchandik@gitam.in>
2024-05-25 18:59:39 +0530

    refactor: Remove unused useEffect import in useData.js

--- src/services/useData.js
+++ src/services/useData.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import Raxios from './axiosHelper';
 
 const DataContext = React.createContext();
