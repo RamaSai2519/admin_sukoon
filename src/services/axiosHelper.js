44644f1090855a33cb3143e04b6b68ca98bd4422
Rama Sathya Sai <rchandik@gitam.in>
2024-05-28 05:13:33 +0530

    chore: Update TypeScript dependency to version 4.9.5

--- src/services/axiosHelper.js
+++ src/services/axiosHelper.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const Raxios = axios.create({
+  // baseURL: 'https://m196vr75-8080.inc1.devtunnels.ms/'
   baseURL: 'https://apiadmin.sukoon.love',
   // baseURL: 'http://127.0.0.1:8080',
 });
