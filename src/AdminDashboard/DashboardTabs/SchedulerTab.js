a80ea1b7c96e61117466dfadaad600729ed871a6
Rama Sathya Sai <rchandik@gitam.in>
2024-06-18 12:53:22 +0530

    chore: Update API base URL in axiosHelper.js and fetchShorts function
    feat: Enable image lazy loading in GamesTab and ContentTab
    
    fix: Display error message when API request fails in GamesTab
    
    refactor: Update LeadsPopup component to use local state for data and not reload on edit

--- src/AdminDashboard/DashboardTabs/SchedulerTab.js
+++ src/AdminDashboard/DashboardTabs/SchedulerTab.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Select, DatePicker, Form, Button, Table, ConfigProvider, theme } from "antd";
+import { Select, DatePicker, Form, Button, Table, Checkbox, ConfigProvider, theme } from "antd";
 import Raxios from "../../services/axiosHelper";
 import { useSchedules, useUsers, useExperts } from "../../services/useData";
 import LazyLoad from "../../components/LazyLoad/lazyload";
@@ -13,6 +13,19 @@ const SchedulerTab = () => {
     const { users } = useUsers();
     const { experts } = useExperts();
     const { Option } = Select;
+
+    const slots = [
+        '15:00 - 15:30',
+        '15:30 - 16:00',
+        '16:00 - 16:30',
+        '16:30 - 17:00',
+        '17:00 - 17:30',
+        '17:30 - 18:00',
+        '18:00 - 18:30',
+        '18:30 - 19:00',
+        '19:00 - 19:30',
+        '19:30 - 20:00'
+    ];
 
     const columns = [
         {
@@ -100,9 +113,15 @@ const SchedulerTab = () => {
             <ConfigProvider theme={{
                 algorithm: darkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
             }}>
-                <div className="mt-2">
-                    <div className="flex items-center justify-center">
-                        <Form name="connect-call" className="grid grid-cols-4 gap-2" onFinish={onConnectFinish}>
+                <div className="flex items-center justify-center gap-4 h-full ">
+                    <div className="w-3/4">
+                        {loading ? <Loading /> :
+                            <Table dataSource={schedules.reverse()} columns={columns} />
+                        }
+                    </div>
+                    <div className="flex flex-col h-full border-l-2 dark:border-lightBlack pl-2 justify-center">
+                        <h1 className="text-2xl font-bold mb-3">Connect a Call</h1>
+                        <Form name="connect-call" className="grid grid-cols-2 gap-2 w-full border-b-2 dark:border-lightBlack pb-4" onFinish={onConnectFinish}>
                             <Form.Item
                                 name={"user"}
                                 rules={[
@@ -114,7 +133,7 @@ const SchedulerTab = () => {
                             >
                                 <Select
                                     id="user"
-                                    style={{ width: "100%" }}
+                                    className="w-full"
                                     showSearch
                                     placeholder="Select User"
                                     optionFilterProp="children"
@@ -137,7 +156,7 @@ const SchedulerTab = () => {
                             >
                                 <Select
                                     id="expert"
-                                    style={{ width: "100%" }}
+                                    className="w-full"
                                     showSearch
                                     placeholder="Select Expert"
                                     optionFilterProp="children"
@@ -148,86 +167,93 @@ const SchedulerTab = () => {
                                     {expertOptions}
                                 </Select>
                             </Form.Item>
-                            <Button htmlType="submit" style={{ width: "100%" }}>
+                            <Button htmlType="submit" className="w-full">
                                 Connect Now
                             </Button>
                         </Form>
-                    </div>
 
-                    <Form name="schedule-call" className="grid grid-cols-4 gap-2 mt-3" onFinish={onScheduleFinish}>
-                        <Form.Item
-                            name={"user"}
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Please select a user",
-                                },
-                            ]}
-                        >
-                            <Select
-                                id="user"
-                                style={{ width: "100%" }}
-                                showSearch
-                                placeholder="Select User"
-                                optionFilterProp="children"
-                                filterOption={(input, option) =>
-                                    option.children && option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                                }
+                        <h1 className="text-2xl font-bold mt-4">Schedule a Call</h1>
+                        <Form name="schedule-call" className="grid grid-cols-2 gap-2 mt-3" onFinish={onScheduleFinish}>
+                            <Form.Item
+                                name={"user"}
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please select a user",
+                                    },
+                                ]}
                             >
-                                {userOptions}
-                            </Select>
-                        </Form.Item>
-                        <Form.Item
-                            className="m-0"
-                            name={"expert"}
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Please select an expert",
-                                },
-                            ]}
-                        >
-                            <Select
-                                id="expert"
-                                style={{ width: "100%" }}
-                                showSearch
-                                placeholder="Select Expert"
-                                optionFilterProp="children"
-                                filterOption={(input, option) =>
-                                    option.children && option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                                }
+                                <Select
+                                    id="user"
+                                    className="w-full"
+                                    showSearch
+                                    placeholder="Select User"
+                                    optionFilterProp="children"
+                                    filterOption={(input, option) =>
+                                        option.children && option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                    }
+                                >
+                                    {userOptions}
+                                </Select>
+                            </Form.Item>
+                            <Form.Item
+                                className="m-0"
+                                name={"expert"}
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please select an expert",
+                                    },
+                                ]}
                             >
-                                {expertOptions}
-                            </Select>
-                        </Form.Item>
-
-                        <Form.Item
-                            className="m-0"
-                            name={"datetime"}
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Please select a date and time",
-                                },
-                            ]}
-                        >
-                            <DatePicker
-                                id="datetime"
-                                style={{ width: "100%" }}
-                                showTime
-                                format="YYYY-MM-DD HH:mm:ss"
-                            />
-                        </Form.Item>
-
-                        <Button htmlType="submit" style={{ width: "100%" }}>
-                            Schedule
-                        </Button>
-                    </Form>
-
-                    <div className="m-0">
-                        {loading ? <Loading /> :
-                            <Table dataSource={schedules.reverse()} columns={columns} />
-                        }
+                                <Select
+                                    id="expert"
+                                    className="w-full"
+                                    showSearch
+                                    placeholder="Select Expert"
+                                    optionFilterProp="children"
+                                    filterOption={(input, option) =>
+                                        option.children && option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                    }
+                                >
+                                    {expertOptions}
+                                </Select>
+                            </Form.Item>
+                            <Form.Item
+                                className="m-0"
+                                name={"datetime"}
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please select a date and time",
+                                    },
+                                ]}
+                            >
+                                <DatePicker
+                                    id="datetime"
+                                    className="w-full"
+                                    showTime
+                                    format="YYYY-MM-DD HH:mm:ss"
+                                    onInput={(e) => console.log(e)}
+                                />
+                            </Form.Item>
+                            {/* {slots &&
+                                <Checkbox.Group className="grid grid-cols-3 gap-2 bg-lightBlack p-2 rounded-xl my-2" style={{ "gridColumn": "1 / span 2" }} options={slots} />
+                            } */}
+                            <Button htmlType="submit" className="w-full">
+                                Schedule
+                            </Button>
+                        </Form>
+                        <div>
+                            {/* <h1 className="text-2xl font-bold mt-4">Available Slots</h1> */}
+                            {/* <div className="grid gap-2 mt-3">
+                                {slots.map(slot => (
+                                    <div key={slot} className="w-fit p-2 rounded-xl border-2 dark:border-lightBlack">
+                                        {slot}
+                                    </div>
+                                ))}
+                            </div> */}
+                        </div>
                     </div>
                 </div>
             </ConfigProvider>
