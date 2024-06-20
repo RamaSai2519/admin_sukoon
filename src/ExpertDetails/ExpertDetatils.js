48599d2bf51c4b5307fb29332c0a863de743149d
Rama Sathya Sai <rchandik@gitam.in>
2024-06-20 22:01:50 +0530

    chore: Update API base URL in axiosHelper.js and fetchShorts function
    feat: Enable image lazy loading in GamesTab and ContentTab
    
    fix: Display error message when API request fails in GamesTab
    
    refactor: Update LeadsPopup component to use local state for data and not reload on edit

--- src/ExpertDetails/ExpertDetatils.js
+++ src/ExpertDetails/ExpertDetatils.js
@@ -3,7 +3,9 @@ import Raxios from '../services/axiosHelper';
 import { useParams, Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import { useCategories } from '../services/useData';
-import { Select, ConfigProvider, theme } from 'antd';
+import { Select, Table, ConfigProvider, theme } from 'antd';
+import EditableTimeCell from '../components/EditableTimeCell'; // Import the new component
+import Loading from '../components/Loading/loading';
 import './ExpertDetails.css';
 
 const { Option } = Select;
@@ -25,11 +27,25 @@ const ExpertDetails = () => {
   const [callsShare, setCallsShare] = useState('');
   const [editMode, setEditMode] = useState(false);
   const { allCategories, fetchCategories } = useCategories();
+  const [timings, setTimings] = useState([]);
+  const [loading, setLoading] = useState(false);
   const dropdownRef = useRef(null);
   const darkMode = localStorage.getItem('darkMode') === 'true';
 
   useEffect(() => {
+    if (darkMode) {
+      localStorage.setItem('darkMode', 'true');
+      document.body.classList.add('dark');
+    } else {
+      localStorage.setItem('darkMode', 'false');
+      document.body.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  useEffect(() => {
+    setLoading(true);
     fetchCategories();
+    fetchTimings(expertId);
     Raxios.get(`/expert/experts/${expertId}`)
       .then(response => {
         setExpert(response.data);
@@ -45,12 +61,24 @@ const ExpertDetails = () => {
         setScore(response.data.score);
         setRepeatScore(response.data.repeat_score);
         setTotalScore(response.data.total_score);
+        setLoading(false);
       })
       .catch(error => {
         console.error('Error fetching expert details:', error);
       });
-      // eslint-disable-next-line
+    // eslint-disable-next-line
   }, [expertId]);
+
+  const fetchTimings = (expertId) => {
+    Raxios.get(`/data/timings?expert=${expertId}`)
+      .then(response => {
+        console.log('Timings:', response.data);
+        setTimings(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching expert timings:', error);
+      });
+  };
 
   const handleCategoryChange = (value) => {
     setCategories(value);
@@ -95,167 +123,248 @@ const ExpertDetails = () => {
       });
   };
 
+  const columns = [
+    {
+      title: 'Day',
+      dataIndex: 'day',
+      key: 'day',
+      editable: false,
+    },
+    {
+      title: 'Start Time 1',
+      dataIndex: 'PrimaryStartTime',
+      key: 'PrimaryStartTime',
+      editable: true,
+    },
+    {
+      title: 'End Time 1',
+      dataIndex: 'PrimaryEndTime',
+      key: 'PrimaryEndTime',
+      editable: true,
+    },
+    {
+      title: 'Start Time 2',
+      dataIndex: 'SecondaryStartTime',
+      key: 'SecondaryStartTime',
+      editable: true,
+    },
+    {
+      title: 'End Time 2',
+      dataIndex: 'SecondaryEndTime',
+      key: 'SecondaryEndTime',
+      editable: true,
+    },
+  ];
+
+  const handleSave = async (row) => {
+    Raxios.post(`/data/timings`, {
+      expertId: expertId,
+      row
+    })
+      .then(() => {
+        window.alert('Timings updated successfully!');
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error('Error updating timings:', error);
+        window.alert('Error updating timings:', error);
+      });
+  };
+
+  const components = {
+    body: {
+      cell: EditableTimeCell, // Use the new EditableTimeCell component
+    },
+  };
+
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave,
+      }),
+    };
+  });
+
+  if (loading) {
+    return <div className='min-h-screen w-full'>
+      <Loading />
+    </div>;
+  }
+
   return (
     <ConfigProvider theme={
       {
         algorithm: darkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
       }
     }>
-      <div className='details-container'>
-        {expert && (
-          <div className='content-container'>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', margin: '20px' }}>
-              <h1>Expert Details</h1>
-              <button className='back-button' onClick={() => window.history.back()}>
-                <FaArrowLeft className="back-icon" />
-              </button>
+      {expert && (
+        <div className='h3-darkgrey'>
+          <div className='flex flex-row justify-between items-center p-5 overflow-auto'>
+            <h1>Expert Details</h1>
+            <button className='back-button' onClick={() => window.history.back()}>
+              <FaArrowLeft className="back-icon" />
+            </button>
+          </div>
+          <div className="grid-tile-2">
+            <img src={profile} alt="Expert Profile" className='max-h-52' />
+          </div>
+          <div className="grid grid-cols-2 p-5 overflow-auto">
+            <div className='grid-tile'>
+              <h3>Name</h3>
+              {editMode ? (
+                <p><input type="text" value={name} onChange={(e) => setName(e.target.value)} /></p>
+              ) : (
+                <h2>{name}</h2>
+              )}
             </div>
-            <div className="grid-container">
-              <div className="grid-tile-2">
-                <img src={profile} alt="Expert Profile" />
+            <div className='grid-tile'>
+              <h3>Phone Number</h3>
+              {editMode ? (
+                <p><input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} /></p>
+              ) : (
+                <h2>{phoneNumber}</h2>
+              )}
+            </div>
+            <div className='grid grid-cols-4'>
+              <div className='grid-tile'>
+                <h3>Score</h3>
+                {editMode ? (
+                  <p><input type="number" value={score} onChange={(e) => setScore(e.target.value)} /></p>
+                ) : (
+                  <h2>{score}</h2>
+                )}
               </div>
-              <div className="grid-item">
-                <div className='grid-tile'>
-                  <h3>Name</h3>
+              <div className='grid-tile'>
+                <h3>Repeat Score</h3>
+                {editMode ? (
+                  <p><input type="number" value={repeatScore} onChange={(e) => setRepeatScore(e.target.value)} /></p>
+                ) : (
+                  <h2>{repeatScore}</h2>
+                )}
+              </div>
+              <div className='grid-tile'>
+                <h3>Calls Share</h3>
+                {editMode ? (
+                  <p><input type="number" value={callsShare} onChange={(e) => setCallsShare(e.target.value)} /></p>
+                ) : (
+                  <h2>{callsShare}%</h2>
+                )}
+              </div>
+              <div className='grid-tile'>
+                <h3>Total Score</h3>
+                {editMode ? (
+                  <p><input type="number" value={totalScore} onChange={(e) => setTotalScore(e.target.value)} /></p>
+                ) : (
+                  <h2>{totalScore}</h2>
+                )}
+              </div>
+            </div>
+            <div className='grid-tile'>
+              <h3>Categories</h3>
+              <div ref={dropdownRef}>
+                <div>
                   {editMode ? (
-                    <p><input type="text" value={name} onChange={(e) => setName(e.target.value)} /></p>
+                    <Select
+                      mode="multiple"
+                      style={{ width: '100%' }}
+                      placeholder="Select Categories"
+                      onChange={handleCategoryChange}
+                      value={categories}
+                    >
+                      {allCategories.map(category => (
+                        <Option key={category._id} value={category}>
+                          {category.name}
+                        </Option>
+                      ))}
+                    </Select>
                   ) : (
-                    <h2>{name}</h2>
+                    <h2>{categories.length > 0 ? categories.join(', ') : 'No categories'}</h2>
                   )}
-                </div>
-                <div className='grid-tile'>
-                  <h3>Phone Number</h3>
-                  {editMode ? (
-                    <p><input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} /></p>
-                  ) : (
-                    <h2>{phoneNumber}</h2>
-                  )}
-                </div>
-                <div className='grid grid-cols-4'>
-                  <div className='grid-tile'>
-                    <h3>Score</h3>
-                    {editMode ? (
-                      <p><input type="number" value={score} onChange={(e) => setScore(e.target.value)} /></p>
-                    ) : (
-                      <h2>{score}</h2>
-                    )}
-                  </div>
-                  <div className='grid-tile'>
-                    <h3>Repeat Score</h3>
-                    {editMode ? (
-                      <p><input type="number" value={repeatScore} onChange={(e) => setRepeatScore(e.target.value)} /></p>
-                    ) : (
-                      <h2>{repeatScore}</h2>
-                    )}
-                  </div>
-                  <div className='grid-tile'>
-                    <h3>Calls Share</h3>
-                    {editMode ? (
-                      <p><input type="number" value={callsShare} onChange={(e) => setCallsShare(e.target.value)} /></p>
-                    ) : (
-                      <h2>{callsShare}%</h2>
-                    )}
-                  </div>
-                  <div className='grid-tile'>
-                    <h3>Total Score</h3>
-                    {editMode ? (
-                      <p><input type="number" value={totalScore} onChange={(e) => setTotalScore(e.target.value)} /></p>
-                    ) : (
-                      <h2>{totalScore}</h2>
-                    )}
-                  </div>
-                </div>
-                <div className='grid-tile'>
-                  <h3>Categories</h3>
-                  <div ref={dropdownRef}>
-                    <div>
-                      {editMode ? (
-                        <Select
-                          mode="multiple"
-                          style={{ width: '100%' }}
-                          placeholder="Select Categories"
-                          onChange={handleCategoryChange}
-                          value={categories}
-                        >
-                          {allCategories.map(category => (
-                            <Option key={category._id} value={category}>
-                              {category.name}
-                            </Option>
-                          ))}
-                        </Select>
-                      ) : (
-                        <h2>{categories.length > 0 ? categories.join(', ') : 'No categories'}</h2>
-                      )}
-                    </div>
-                  </div>
-                </div>
-                <div className='grid-tile'>
-                  <h3>Status</h3>
-                  <div className="toggle-container">
-                    <h2 className="status-label">{status === 'online' ? 'Online' : 'Offline'}</h2>
-                    <label className="switch">
-                      <input
-                        type="checkbox"
-                        checked={status === 'online'}
-                        onChange={() => handleUpdate(status === 'offline' ? 'online' : 'offline')}
-                      />
-                      <span className="slider round"></span>
-                    </label>
-                  </div>
-                </div>
-                <div className='grid-tile'>
-                  <h3>Languages</h3>
-                  {editMode ? (
-                    <p><input type="text" value={languages} onChange={(e) => setLanguages(e.target.value)} /></p>
-                  ) : (
-                    <h2>{languages}</h2>
-                  )}
-                </div>
-                <div className='grid-tile'>
-                  <h3>Topics</h3>
-                  {editMode ? (
-                    <p><input type="text" value={topics} onChange={(e) => setTopics(e.target.value)} /></p>
-                  ) : (
-                    <h2>{topics}</h2>
-                  )}
-                </div>
-                <div className='grid-tile'>
-                  <h3>Description</h3>
-                  {editMode ? (
-                    <p><input type="text" value={description} onChange={(e) => setDescription(e.target.value)} /></p>
-                  ) : (
-                    <h2>{description}</h2>
-                  )}
-                </div>
-                <div className='edit-button-container'>
-                  {editMode ? (
-                    <>
-                      <button className='update-button' onClick={() => setEditMode(false)}>Cancel</button>
-                      <button className='update-button' onClick={() => handleUpdate(status)}>Update Details</button>
-                    </>
-                  ) : (
-                    <>
-                      <button className='update-button' onClick={() => setEditMode(true)}>Edit Details</button>
-                    </>
-                  )}
-                  <Link
-                    to={{
-                      pathname: `/admin/experts/${expertId}/report`
-                    }}
-                    style={{ textDecoration: 'none', color: 'inherit' }}
-                  >
-                    <button className='update-button'>View Detailed Scores</button>
-                  </Link>
-                  <button className='update-button' style={{ backgroundColor: 'red' }} onClick={handleDelete}>Delete Expert</button>
                 </div>
               </div>
+            </div>
+            <div className='grid-tile'>
+              <h3>Status</h3>
+              <div className="toggle-container">
+                <h2 className="status-label">{status === 'online' ? 'Online' : 'Offline'}</h2>
+                <label className="switch">
+                  <input
+                    type="checkbox"
+                    checked={status === 'online'}
+                    onChange={() => handleUpdate(status === 'offline' ? 'online' : 'offline')}
+                  />
+                  <span className="slider round"></span>
+                </label>
+              </div>
+            </div>
+            <div className='grid-tile'>
+              <h3>Languages</h3>
+              {editMode ? (
+                <p><input type="text" value={languages} onChange={(e) => setLanguages(e.target.value)} /></p>
+              ) : (
+                <h2>{languages}</h2>
+              )}
+            </div>
+            <div className='grid-tile'>
+              <h3>Topics</h3>
+              {editMode ? (
+                <p><input type="text" value={topics} onChange={(e) => setTopics(e.target.value)} /></p>
+              ) : (
+                <h2>{topics}</h2>
+              )}
+            </div>
+            <div className='grid-tile'>
+              <h3>Description</h3>
+              {editMode ? (
+                <p><input type="text" value={description} onChange={(e) => setDescription(e.target.value)} /></p>
+              ) : (
+                <h2>{description}</h2>
+              )}
+            </div>
+            <div className='grid-tile'>
+              <h3>Timings</h3>
+              <Table
+                components={components}
+                columns={mergedColumns}
+                dataSource={timings}
+                pagination={false}
+                rowKey={(record) => record._id}
+              />
+            </div>
+            <div className='edit-button-container'>
+              {editMode ? (
+                <>
+                  <button className='update-button' onClick={() => setEditMode(false)}>Cancel</button>
+                  <button className='update-button' onClick={() => handleUpdate(status)}>Update Details</button>
+                </>
+              ) : (
+                <>
+                  <button className='update-button' onClick={() => setEditMode(true)}>Edit Details</button>
+                </>
+              )}
+              <Link
+                to={{
+                  pathname: `/admin/experts/${expertId}/report`
+                }}
+                style={{ textDecoration: 'none', color: 'inherit' }}
+              >
+                <button className='update-button'>View Detailed Scores</button>
+              </Link>
+              <button className='update-button' style={{ backgroundColor: 'red' }} onClick={handleDelete}>Delete Expert</button>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </ConfigProvider>
   );
-
 };
 
 export default ExpertDetails;
