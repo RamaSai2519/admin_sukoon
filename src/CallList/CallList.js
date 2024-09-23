84ea7c15bb5d5a16a7c94993f6f4958ed833a47b
Rama Sathya Sai <rchandik@gitam.in>
2024-09-24 00:58:30 +0530

    refactor: Add export functionality to CallsTable component

--- src/CallList/CallList.js
+++ src/CallList/CallList.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { raxiosFetchData } from '../services/fetchData';
 import CallsTableComponent from '../components/CallsTable';
+import { downloadExcel } from '../Utils/exportHelper';
+import { Button } from 'antd';
 
 const CallsTable = () => {
   const searchInputRef = useRef(null);
@@ -13,6 +15,7 @@ const CallsTable = () => {
   );
   const [pageSize, setPageSize] = useState(10);
   const [totalItems, setTotalItems] = useState(0);
+  const [buttonLoading, setButtonLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -26,17 +29,29 @@ const CallsTable = () => {
     setPageSize(pageSize);
   };
 
+  const exportData = async () => {
+    setButtonLoading(true);
+    const fileName = 'calls.xlsx';
+    let callsData = await raxiosFetchData(1, totalItems, null, null, '/call', { dest: 'list' });
+    callsData = callsData.data;
+    await downloadExcel(callsData, fileName);
+    setButtonLoading(false);
+  }
+
   return (
-    <CallsTableComponent
-      data={data}
-      loading={loading}
-      searchText={searchText}
-      setSearchText={setSearchText}
-      searchedColumn={searchedColumn}
-      setSearchedColumn={setSearchedColumn}
-      searchInputRef={searchInputRef}
-      pagination={{ current: currentPage, pageSize, total: totalItems, onChange: handleTableChange }}
-    />
+    <div className='flex flex-col items-end pt-5'>
+      <Button loading={buttonLoading} onClick={exportData}>Export</Button>
+      <CallsTableComponent
+        data={data}
+        loading={loading}
+        searchText={searchText}
+        setSearchText={setSearchText}
+        searchedColumn={searchedColumn}
+        setSearchedColumn={setSearchedColumn}
+        searchInputRef={searchInputRef}
+        pagination={{ current: currentPage, pageSize, total: totalItems, onChange: handleTableChange }}
+      />
+    </div>
   );
 };
 
