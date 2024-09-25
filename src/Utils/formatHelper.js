b4da86bc4e9a284c9213ce3741b5fd11f412d7de
Rama Sathya Sai <rchandik@gitam.in>
2024-09-25 14:08:24 +0530

    Refactor formatHelper.js: Update convertToIST function to handle invalid dates

--- src/Utils/formatHelper.js
+++ src/Utils/formatHelper.js
@@ -1,26 +1,33 @@
 import { format } from 'date-fns';
 
-// Convert time to IST
 export const convertToIST = (time) => {
     let utcDate = new Date(time);
 
-    // Get the time in milliseconds and add the IST offset (5 hours 30 minutes)
-    let istOffset = 5.5 * 60 * 60 * 1000; // 5 hours 30 minutes in milliseconds
+    let istOffset = 5.5 * 60 * 60 * 1000;
     let istDate = new Date(utcDate.getTime() + istOffset);
 
     return istDate === 'Invalid Date' ? (time || '') : istDate;
 };
 
-// Format time with date and time details
 export const formatTime = (time) => {
     const istDate = convertToIST(time);
-    const newTime = format(istDate, 'd MMM yyyy, h:mm:ss a');
-    return newTime === 'Invalid Date' ? (time || '') : newTime;
+    try {
+        const newDate = format(istDate, 'd MMM yyyy, h:mm:ss a');
+        return newDate;
+    } catch (error) {
+        console.log('error', error);
+        return istDate;
+        
+    }
 };
 
-// Format only the date, with an optional year
 export const formatDate = (time, year = true) => {
     const istDate = convertToIST(time);
-    const newDate = year ? format(istDate, 'd MMM yyyy') : format(istDate, 'd MMM');
-    return newDate === 'Invalid Date' ? (time || '') : newDate;
+    try {
+        const newDate = year ? format(istDate, 'd MMM yyyy') : format(istDate, 'd MMM');
+        return newDate;
+    } catch (error) {
+        console.log('error', error);
+        return istDate;
+    }
 };
