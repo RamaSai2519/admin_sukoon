d1537656aa0909108d8cab0f19b5df8d79ef55b0
Rama Sathya Sai <rchandik@gitam.in>
2024-09-25 12:24:22 +0530

    Refactor formatHelper.js: Update convertToIST function to accept optional reqDateObj parameter

--- src/Utils/formatHelper.js
+++ src/Utils/formatHelper.js
@@ -1,21 +1,25 @@
 import moment from 'moment-timezone';
 
 // Convert time to IST
-export const convertToIST = (time) => {
+export const convertToIST = (time, reqDateObj = true) => {
     const value = moment(time).tz('Asia/Kolkata');
-    return new Date(value.format());
+    if (reqDateObj) {
+        return new Date(value.format());
+    } else {
+        return value
+    }
 };
 
 // Format time with date and time details
 export const formatTime = (time) => {
-    const istDate = convertToIST(time);
+    const istDate = convertToIST(time, false);
     const newTime = istDate.format('D MMM YYYY, h:mm:ss A');
     return newTime === 'Invalid Date' ? (time || '') : newTime;
 };
 
 // Format only the date, with an optional year
 export const formatDate = (time, year = true) => {
-    const istDate = convertToIST(time);
+    const istDate = convertToIST(time, false);
     const newDate = year ? istDate.format('D MMM YYYY') : istDate.format('D MMM');
     return newDate === 'Invalid Date' ? (time || '') : newDate;
 };
