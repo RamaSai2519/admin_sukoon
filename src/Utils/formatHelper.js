733f60c405a3fbc8ac3dfdd13d25785ba77167fc
Rama Sathya Sai <rchandik@gitam.in>
2024-09-25 12:35:35 +0530

    Update package dependencies

--- src/Utils/formatHelper.js
+++ src/Utils/formatHelper.js
@@ -1,25 +1,26 @@
-import moment from 'moment-timezone';
+import { format } from 'date-fns';
 
 // Convert time to IST
-export const convertToIST = (time, reqDateObj = true) => {
-    const value = moment(time).tz('Asia/Kolkata');
-    if (reqDateObj) {
-        return new Date(value.format());
-    } else {
-        return value
-    }
+export const convertToIST = (time) => {
+    let utcDate = new Date(time);
+
+    // Get the time in milliseconds and add the IST offset (5 hours 30 minutes)
+    let istOffset = 5.5 * 60 * 60 * 1000; // 5 hours 30 minutes in milliseconds
+    let istDate = new Date(utcDate.getTime() + istOffset);
+
+    return istDate === 'Invalid Date' ? (time || '') : istDate;
 };
 
 // Format time with date and time details
 export const formatTime = (time) => {
-    const istDate = convertToIST(time, false);
-    const newTime = istDate.format('D MMM YYYY, h:mm:ss A');
+    const istDate = convertToIST(time);
+    const newTime = format(istDate, 'd MMM yyyy, h:mm:ss a');
     return newTime === 'Invalid Date' ? (time || '') : newTime;
 };
 
 // Format only the date, with an optional year
 export const formatDate = (time, year = true) => {
-    const istDate = convertToIST(time, false);
-    const newDate = year ? istDate.format('D MMM YYYY') : istDate.format('D MMM');
+    const istDate = convertToIST(time);
+    const newDate = year ? format(istDate, 'd MMM yyyy') : format(istDate, 'd MMM');
     return newDate === 'Invalid Date' ? (time || '') : newDate;
 };
