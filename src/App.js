6a35e67c6c1498170b011854c928811675beb588
Rama Sathya Sai <rchandik@gitam.in>
2024-09-27 09:01:03 +0530

    Update app version to 17.0.3

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import EventDetails from './EventDetails';
 import './App.css';
 
 const App = () => {
-  const appVersion = '17.0.2';
+  const appVersion = '17.0.3';
   const [isLoggedIn, setIsLoggedIn] = useState(
     localStorage.getItem('isLoggedIn') === 'true'
   );
