32c33d7810ef1e117593a58836126567503c731b
Rama Sathya Sai <rchandik@gitam.in>
2024-09-28 17:26:52 +0530

    Update app version to 17.0.6 and handle invalid dates in convertToIST function Refactor ExpertDetails.js: Display phone number instead of input field

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import EventDetails from './EventDetails';
 import './App.css';
 
 const App = () => {
-  const appVersion = '17.0.5';
+  const appVersion = '17.0.6';
   const [isLoggedIn, setIsLoggedIn] = useState(
     localStorage.getItem('isLoggedIn') === 'true'
   );
