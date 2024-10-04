fd99a267c51620b52e17f20c8e824f0a9f779933
Rama Sathya Sai <rchandik@gitam.in>
2024-10-04 18:31:03 +0530

    Refactor FaxiosPost function to handle notification messages

--- src/helpers/faxios.js
+++ src/helpers/faxios.js
@@ -4,10 +4,12 @@ import Faxios from "../services/raxiosHelper";
 export const FaxiosPost = async (url, data, isNotify = false) => {
     try {
         const response = await Faxios.post(url, data);
-        if (isNotify && response.status === 200) {
-            message.success(response.msg);
-        } else {
-            message.error(response.msg);
+        if (isNotify) {
+            if (response.status === 200) {
+                message.success(response.msg);
+            } else {
+                message.error(response.msg);
+            }
         }
         return response;
     } catch (error) {
