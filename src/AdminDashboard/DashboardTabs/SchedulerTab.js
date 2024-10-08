986cba60cf6f87d3f0db9332078acc8ce7870b68
Rama Sathya Sai <rchandik@gitam.in>
2024-10-08 05:46:32 +0530

    Refactor: Add 'Source' column to CallsTable and SchedulesTable components

--- src/AdminDashboard/DashboardTabs/SchedulerTab.js
+++ src/AdminDashboard/DashboardTabs/SchedulerTab.js
@@ -1,45 +1,33 @@
-import { Select, DatePicker, Form, Button, Table, message } from "antd";
+import React, { useEffect, useState } from "react";
+import { RaxiosPost } from "../../services/fetchData";
+import LazyLoad from "../../components/LazyLoad/lazyload";
+import InternalToggle from "../../components/InternalToggle";
+import SchedulesTable from "../../components/SchedulesTable";
 import { useUsers, useExperts } from "../../services/useData";
 import { generateOptions } from "../../Utils/antSelectHelper";
-import getColumnSearchProps from "../../Utils/antTableHelper";
-import InternalToggle from "../../components/InternalToggle";
-import React, { useEffect, useRef, useState } from "react";
-import LazyLoad from "../../components/LazyLoad/lazyload";
-import Loading from "../../components/Loading/loading";
-import { formatTime } from "../../Utils/formatHelper";
-import { RaxiosPost } from "../../services/fetchData";
-import Raxios from "../../services/axiosHelper";
+import { Select, DatePicker, Form, Button, message } from "antd";
 
 const SchedulerTab = () => {
-    // const [slots, setSlots] = useState([]);
-    // const [sValues, setSValues] = useState({ user: "", expert: "", datetime: "" });
-    // const [fslot, setFSlot] = useState([]); // Final slot state
-    // const [selectedSlot, setSelectedSlot] = useState(null); // Selected slot state
-
-    const searchInputRef = useRef(null);
     const { users, fetchUsers } = useUsers();
+    const { experts, fetchExperts } = useExperts();
+    const [schedules, setSchedules] = useState([]);
+    const [rLoading, setRLoading] = useState(false);
     const [loading, setLoading] = useState(false);
     const [disable, setDisable] = useState(false);
-    const [schedules, setSchedules] = useState([]);
-    const { experts, fetchExperts } = useExperts();
-    const [searchText, setSearchText] = useState('');
-    const [searchedColumn, setSearchedColumn] = useState('');
-    const [responseLoading, setResponseLoading] = useState(false);
     const [internalView, setInternalView] = useState(
         localStorage.getItem('internalView') === 'true' ? true : false
     );
     const { Option } = Select;
 
     const fetchSchedules = async () => {
-        setLoading(true);
+        setRLoading(true);
         const response = await RaxiosPost('/schedules', { action: 'get' });
         setSchedules(response.data.data);
-        setLoading(false);
-    }
+        setRLoading(false);
+    };
 
     useEffect(() => {
         fetchSchedules();
-
     }, []);
 
     const fetchUsersAndExperts = async () => {
@@ -54,238 +42,114 @@ const SchedulerTab = () => {
         // eslint-disable-next-line
     }, [internalView]);
 
-    const createColumn = (title, dataIndex, key, sorter, render, defaultSortOrder) => {
-        return {
-            title,
-            dataIndex,
-            key,
-            ...getColumnSearchProps(dataIndex, title, searchText, setSearchText, searchedColumn, setSearchedColumn, searchInputRef),
-            ...(sorter && { sorter: sorter }),
-            ...(render && { render: render }),
-            ...(defaultSortOrder && { defaultSortOrder: defaultSortOrder }),
-        };
+    const handleCallTrigger = async (values) => {
+        setLoading(true);
+        await RaxiosPost('/call', {
+            user_id: values.user,
+            expert_id: values.expert,
+            user_requested: values.user_requested === "Yes"
+        }, true);
+        setLoading(false);
     };
 
-    const columns = [
-        createColumn("User", "user", "user"),
-        createColumn("Expert", "expert", "expert"),
-        createColumn("Date & Time", "datetime", "datetime",
-            (a, b) => new Date(a.datetime) - new Date(b.datetime),
-            (record) => formatTime(record), "descend"),
-        createColumn("Status", "status", "status"),
-        {
-            title: "Action", key: "action",
-            render: (_, record) => {
-                if (record.isDeleted) {
-                    return <Button disabled>Deleted</Button>;
-                } else {
-                    return <Button loading={responseLoading} onClick={() => handleDelete(record)}>Delete</Button>
-                }
-            }
-        },
+    const onScheduleFinish = async (values) => {
+        setLoading(true);
+        const selectedDateTime = values.datetime;
+        if (selectedDateTime <= new Date()) {
+            message.error("Selected time has already passed. Please select a future time.");
+        } else {
+            const formattedDate = new Date(values.datetime).toISOString().split('.')[0] + "Z";
+            const meta = JSON.stringify({ expertId: values.expert, userId: values.user });
+            await RaxiosPost('/create_scheduled_job',
+                { job_type: 'CALL', job_time: formattedDate, status: 'PENDING', request_meta: meta, user_requested: values.user_requested === "Yes" },
+                true
+            );
+            fetchSchedules();
+        }
+        setLoading(false);
+    };
+
+    const CallScheduleForm = ({ onFinish, loading, fields }) => {
+        return (
+            <Form
+                name="call-schedule-form"
+                className="grid grid-cols-2 gap-2 mt-3"
+                onFinish={onFinish}
+            >
+                {fields.filter(field => !field.actionText).map((field, index) => (
+                    <Form.Item
+                        key={index}
+                        name={field.name}
+                        rules={field.rules}
+                    >
+                        {field.type === "select" ? (
+                            <Select
+                                className="w-full" showSearch
+                                placeholder={field.placeholder} optionFilterProp="children"
+                                filterOption={(input, option) =>
+                                    option.children.toLowerCase().includes(input.toLowerCase())
+                                }
+                            >
+                                {field.generateOption ? generateOptions(field.options, field.optionKey) :
+                                    field.options?.map((option, index) => (
+                                        <Option key={index} value={option}>{option}</Option>
+                                    ))
+                                }
+                            </Select>
+                        ) : field.type === "datetime" ? (
+                            <DatePicker
+                                className="w-full"
+                                format="YYYY-MM-DD HH:mm"
+                                showTime
+                            />
+                        ) : null}
+                    </Form.Item>
+                ))}
+                <Button loading={loading} htmlType="submit" className="w-full col-span-2">
+                    {fields.find(field => field.actionText)?.actionText}
+                </Button>
+            </Form>
+        );
+    };
+
+    const requiredRule = (message) => [{ required: true, message }];
+    const actionField = (actionText) => ({ actionText });
+
+    const userFormField = {
+        name: "user", type: "select", placeholder: "Select User",
+        showSearch: true, options: users, optionKey: "name", generateOption: true,
+        rules: requiredRule("Please select a user")
+    };
+    const expertFormField = {
+        name: "expert", type: "select", placeholder: "Select Expert",
+        showSearch: true, options: experts, optionKey: "name", generateOption: true,
+        rules: requiredRule("Please select an expert")
+    };
+
+    const userRequestedFormField = { name: "user_requested", type: "select", placeholder: "Select User Requested", options: ["Yes", "No"], rules: requiredRule("Please select if user requested or not") };
+
+    const callFormFields = [userFormField, expertFormField, userRequestedFormField, actionField("Connect Now")];
+    const scheduleFormFields = [
+        userFormField,
+        expertFormField,
+        { name: "datetime", type: "datetime", rules: requiredRule("Please select a date and time"), },
+        { name: "duration", type: "select", placeholder: "Select Duration", options: ["30", "60"], rules: requiredRule("Please select a duration") },
+        userRequestedFormField, actionField("Schedule Call")
     ];
-
-    const handleDelete = async (record) => {
-        setResponseLoading(true);
-        try {
-            await RaxiosPost('/schedules', {
-                scheduleId: record.id,
-                action: 'delete'
-            }, true);
-            window.location.reload();
-        } catch (error) {
-            console.error("Error deleting schedule:", error);
-        }
-        setResponseLoading(false);
-    };
-
-    const handleCallTrigger = async (values, endpoint, type_) => {
-        setResponseLoading(true);
-        try {
-            const response = await Raxios.post(endpoint, {
-                user_id: values.user,
-                expert_id: values.expert, type_
-            });
-            if (response.status !== 200) {
-                message.error(response.msg);
-            } else {
-                message.success(response.msg);
-            }
-        } catch (error) {
-            message.error(error.response?.data?.message || 'An error occurred');
-        }
-        setResponseLoading(false);
-    };
-
-    const onScheduleFinish = async values => {
-        setResponseLoading(true);
-        try {
-            const selectedDateTime = values.datetime;
-            const now = new Date();
-            if (selectedDateTime <= now) {
-                window.alert("Selected time has already passed. Please select a future time.");
-            } else {
-                await RaxiosPost('/schedules', {
-                    ...values,
-                    type: "Admin",
-                    action: 'create'
-                }, true);
-                window.location.reload();
-            }
-        } catch (error) {
-            console.error("Error:", error);
-        }
-        setResponseLoading(false);
-    };
 
     return (
         <LazyLoad>
             <div className="flex items-center justify-center gap-4 h-full">
-                <div className="w-3/4">
-                    {loading ? <Loading /> :
-                        <div className="flex flex-col gap-2">
-                            <Table
-                                scroll={{ x: 768 }}
-                                rowKey={(record) => record._id}
-                                dataSource={schedules}
-                                columns={columns}
-                            />
-                        </div>
-                    }
-                </div>
+                <SchedulesTable schedules={schedules} loading={rLoading} />
                 <div className="flex flex-col h-full border-l-2 dark:border-lightBlack pl-2 justify-center">
                     <div className="flex w-full justify-between">
                         <h1 className="text-2xl font-bold mb-3">Connect a Call</h1>
                         <InternalToggle internalView={internalView} setInternalView={setInternalView} disable={disable} />
                     </div>
-                    <Form
-                        name="connect-call"
-                        className="grid grid-cols-2 gap-2 w-full border-b-2 dark:border-lightBlack pb-4"
-                        onFinish={(values) => handleCallTrigger(values, "/call", "call")}
-                    >
-                        <Form.Item
-                            name="user"
-                            rules={[{ required: true, message: "Please select a user" }]}
-                        >
-                            <Select
-                                className="w-full"
-                                showSearch
-                                placeholder="Select User"
-                                optionFilterProp="children"
-                                filterOption={(input, option) =>
-                                    option.children.toLowerCase().includes(input.toLowerCase())
-                                }
-                            >
-                                {generateOptions(users, "name")}
-                            </Select>
-                        </Form.Item>
-                        <Form.Item
-                            name="expert"
-                            rules={[{ required: true, message: "Please select an expert" }]}
-                        >
-                            <Select
-                                className="w-full"
-                                showSearch
-                                placeholder="Select Expert"
-                                optionFilterProp="children"
-                                filterOption={(input, option) =>
-                                    option.children.toLowerCase().includes(input.toLowerCase())
-                                }
-                            >
-                                {generateOptions(experts, "name")}
-                            </Select>
-                        </Form.Item>
-                        <Button loading={responseLoading} htmlType="submit" className="w-full">Connect Now</Button>
-                    </Form>
+                    <CallScheduleForm onFinish={handleCallTrigger} loading={loading} fields={callFormFields} />
 
                     <h1 className="text-2xl font-bold mt-4">Schedule a Call</h1>
-                    <Form
-                        name="schedule-call"
-                        className="grid grid-cols-2 gap-2 mt-3"
-                        onFinish={onScheduleFinish}
-                    >
-                        <Form.Item
-                            name="user"
-                            rules={[{ required: true, message: "Please select a user" }]}
-                        >
-                            <Select
-                                className="w-full"
-                                showSearch
-                                placeholder="Select User"
-                                optionFilterProp="children"
-                                filterOption={(input, option) =>
-                                    option.children.toLowerCase().includes(input.toLowerCase())
-                                }
-                            >
-                                {generateOptions(users, "name")}
-                            </Select>
-                        </Form.Item>
-                        <Form.Item
-                            name="expert"
-                            rules={[{ required: true, message: "Please select an expert" }]}
-                        >
-                            <Select
-                                className="w-full"
-                                showSearch
-                                placeholder="Select Expert"
-                                optionFilterProp="children"
-                                filterOption={(input, option) =>
-                                    option.children.toLowerCase().includes(input.toLowerCase())
-                                }
-                            >
-                                {generateOptions(experts, "name")}
-                            </Select>
-                        </Form.Item>
-                        <Form.Item
-                            name="datetime"
-                            rules={[{ required: true, message: "Please select a date and time" }]}
-                        >
-                            <DatePicker
-                                className="w-full"
-                                format="YYYY-MM-DD HH:mm:ss"
-                                showTime
-                            />
-                        </Form.Item>
-                        <Form.Item
-                            name="duration"
-                            rules={[{ required: true, message: "Please select a duration" }]}
-                        >
-                            <Select
-                                className="w-full"
-                                placeholder="Select Duration"
-                            >
-                                {["30", "60"].map(duration => (
-                                    <Option key={duration} value={duration}>
-                                        {duration} minutes
-                                    </Option>
-                                ))}
-                            </Select>
-                        </Form.Item>
-                        <Button loading={responseLoading} htmlType="submit" className="w-full">Schedule Call</Button>
-                        {/* {slots.length !== 0 ?
-                                <div
-                                    style={{ gridColumn: "1 / span 2" }}
-                                >
-                                    <div className="grid grid-cols-3 gap-2 dark:bg-lightBlack p-2 rounded-xl my-2">
-                                        {slots.map(slot => (
-                                            <Button
-                                                key={slot.label}
-                                                className={`w-full my-2 ${selectedSlot === slot ? 'ant-btn-primary' : ''}`}
-                                                onClick={() => {
-                                                    setSelectedSlot(slot);
-                                                    setFSlot([slot.value]);
-                                                }}
-                                                disabled={!slot.value.available}
-                                            >
-                                                {slot.label}
-                                            </Button>
-                                        ))}
-                                    </div>
-                                    <Button onClick={onFinalSchedule} className="w-full">Schedule</Button>
-                                </div>
-                                : null
-                            } */}
-                    </Form>
+                    <CallScheduleForm onFinish={onScheduleFinish} loading={loading} fields={scheduleFormFields} />
                 </div>
             </div>
         </LazyLoad>
---
+++ src/components/SchedulesTable/index.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef, useState } from "react";
+import { formatTime } from "../../Utils/formatHelper";
+import { RaxiosPost } from "../../services/fetchData";
+import getColumnSearchProps from "../../Utils/antTableHelper";
+import Loading from "../Loading/loading";
+import { Button, Table } from "antd";
+
+const SchedulesTable = ({ schedules, loading }) => {
+    const searchInputRef = useRef(null);
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const [responseLoading, setResponseLoading] = useState(false);
+
+    const createColumn = (title, dataIndex, key, sorter, render, defaultSortOrder) => ({
+        title,
+        dataIndex,
+        key,
+        ...getColumnSearchProps(dataIndex, title, searchText, setSearchText, searchedColumn, setSearchedColumn, searchInputRef),
+        ...(sorter && { sorter }),
+        ...(render && { render }),
+        ...(defaultSortOrder && { defaultSortOrder })
+    });
+
+    const columns = [
+        createColumn("User", "user", "user"),
+        createColumn("Expert", "expert", "expert"),
+        createColumn("Date & Time", "datetime", "datetime",
+            (a, b) => new Date(a.datetime) - new Date(b.datetime),
+            (record) => formatTime(record), "descend"),
+        createColumn("Status", "status", "status"),
+        createColumn('Source', 'source', 'source'),
+        {
+            title: "Action", key: "action",
+            render: (_, record) => (
+                <Button
+                    disabled={record.isDeleted}
+                    loading={responseLoading}
+                    onClick={() => !record.isDeleted && handleDelete(record)}
+                >
+                    {record.isDeleted ? "Canceled" : "Cancel"}
+                </Button>
+            )
+        }
+    ];
+
+    const handleDelete = async (record) => {
+        setResponseLoading(true);
+        await RaxiosPost('/update_scheduled_job', {
+            scheduled_job_id: record.id,
+            action: 'DELETE'
+        }, true);
+        schedules.find(s => s.id === record.id).isDeleted = true;
+        setResponseLoading(false);
+    };
+
+    return (
+        <div className="w-3/4">
+            {loading ? <Loading /> : (
+                <div className="flex flex-col gap-2">
+                    <Table
+                        scroll={{ x: 768 }}
+                        rowKey={(record) => record._id}
+                        dataSource={schedules}
+                        columns={columns}
+                    />
+                </div>
+            )}
+        </div>
+    )
+};
+
+export default SchedulesTable;
