c4df6345b1bd0a40d825ea2f0cbb27502bbddade
RamaSai2519 <rchandik@gitam.in>
2024-11-19 22:09:50 +0530

    feat: Update application name and enhance preconnect link in manifest and index files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import { createRoot } from 'react-dom/client';
 import { DataProvider } from './services/useData';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { FINAL_URL } from './services/axiosHelper';
 import App from './App';
 import './index.css';
 
@@ -11,11 +9,6 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <Router>
-    <Helmet>
-      <title>Sukoon Admin</title>
-      <meta name="description" content="Discover engaging narratives, diverse languages, and the essence of heartfelt conversations in senior living on the sukoon.love. Embrace the wisdom of elders and nurture authentic connections through our enriching content." />
-      <link rel="preconnect" href={FINAL_URL} />
-    </Helmet>
     <DataProvider>
       <App />
     </DataProvider>
