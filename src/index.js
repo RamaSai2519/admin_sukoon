5775d92c5e3f48683a3d5e8c5164f6907648aecb
RamaSai2519 <rchandik@gitam.in>
2024-11-19 21:54:16 +0530

    feat: Add preconnect link for improved API connection performance

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
 import { DataProvider } from './services/useData';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { FINAL_URL } from './services/axiosHelper';
+import App from './App';
+import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -13,6 +14,7 @@ root.render(
     <Helmet>
       <title>Sukoon Admin</title>
       <meta name="description" content="Discover engaging narratives, diverse languages, and the essence of heartfelt conversations in senior living on the sukoon.love. Embrace the wisdom of elders and nurture authentic connections through our enriching content." />
+      <link rel="preconnect" href={FINAL_URL} />
     </Helmet>
     <DataProvider>
       <App />
