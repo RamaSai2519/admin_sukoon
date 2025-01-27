4a6f799109e6d604ebb237eff3d3d99f4afa550f
RamaSai2519 <rchandik@gitam.in>
2025-01-27 11:59:14 +0530

    feat: Enhance UpsertPlanForm with loading state and plan type selection

--- src/components/UpsertPlanForm/index.js
+++ src/components/UpsertPlanForm/index.js
@@ -1,12 +1,20 @@
-import React, { useEffect } from "react";
-import { RaxiosPost } from "../../services/fetchData";
-import { Button, Form, Input, InputNumber, message } from "antd";
+import React, { useEffect, useState } from "react";
+import { raxiosFetchData, RaxiosPost } from "../../services/fetchData";
+import { Button, Form, Input, InputNumber, message, Select } from "antd";
+import Loading from "../Loading/loading";
 
 
 const UpsertPlanForm = ({ plan, setPlan }) => {
     const [form] = Form.useForm();
+    const [loading, setLoading] = useState(false);
+    const [planTypes, setPlanTypes] = useState([]);
+
+    const fetchPlans = async () => {
+        await raxiosFetchData(null, null, setPlanTypes, null, '/actions/user_status_options', { type: 'pay_types' }, setLoading);
+    };
 
     useEffect(() => {
+        fetchPlans();
         form.setFieldsValue({
             name: plan?.name,
             price: plan?.price,
@@ -30,20 +38,27 @@ const UpsertPlanForm = ({ plan, setPlan }) => {
     const createFormItem = (label, name, component, rules) => ({ label, name, component, rules });
 
     const formItems = [
-        createFormItem("Name", "name", <Form.Item name="name"><Input /></Form.Item>, [{ required: true, message: 'Please enter the name' }]),
-        createFormItem("Price", "price", <Form.Item name="price"><InputNumber accept="" /></Form.Item>, [{ required: true, message: 'Please enter the price' }]),
-        createFormItem("Meta", "meta", <Form.Item name="meta"><Input.TextArea /></Form.Item>, [{ required: true, message: 'Please enter the meta' }])
+        createFormItem("Name", "name", <Input />, [{ required: true, message: 'Please enter the name' }]),
+        createFormItem("Price", "price", <InputNumber accept="" />, [{ required: true, message: 'Please enter the price' }]),
+        createFormItem("Meta", "meta", <Input.TextArea />, [{ required: true, message: 'Please enter the meta' }])
     ]
     const formItems1 = [
-        createFormItem("Free Events", "free_events", <Form.Item name="free_events"><InputNumber /></Form.Item>, [{ required: true, message: 'Please enter the free events' }]),
-        createFormItem("Paid Events", "paid_events", <Form.Item name="paid_events"><InputNumber /></Form.Item>, [{ required: true, message: 'Please enter the paid events' }]),
-        createFormItem("Expert Calls", "expert_calls", <Form.Item name="expert_calls"><InputNumber /></Form.Item>, [{ required: true, message: 'Please enter the expert calls' }]),
-        createFormItem("Sarathi Calls", "sarathi_calls", <Form.Item name="sarathi_calls"><InputNumber /></Form.Item>, [{ required: true, message: 'Please enter the sarathi calls' }])
+        createFormItem("Free Events", "free_events", <InputNumber />, [{ required: true, message: 'Please enter the free events' }]),
+        createFormItem("Paid Events", "paid_events", <InputNumber />, [{ required: true, message: 'Please enter the paid events' }]),
+        createFormItem("Expert Calls", "expert_calls", <InputNumber />, [{ required: true, message: 'Please enter the expert calls' }]),
+        createFormItem("Sarathi Calls", "sarathi_calls", <InputNumber />, [{ required: true, message: 'Please enter the sarathi calls' }])
     ]
+
+    const typeFormItem = createFormItem(
+        "Plan Type", "type",
+        <Select className="min-w-14" options={planTypes.map(type => ({ value: type?.type || '', label: type?.desc || '' }))} />,
+        [{ required: true, message: 'Please select the plan type' }]
+    );
 
     const RenderFormItem = ({ item }) => {
         return (
             <Form.Item
+                className="w-full"
                 label={item.label} name={item.name}
                 rules={item.rules} key={item.name}
             >
@@ -51,6 +66,8 @@ const UpsertPlanForm = ({ plan, setPlan }) => {
             </Form.Item>
         )
     }
+
+    if (loading) return <Loading />;
 
     return (
         <Form
@@ -61,13 +78,22 @@ const UpsertPlanForm = ({ plan, setPlan }) => {
         >
             <div className="flex justify-center gap-5">{formItems.map(item => <RenderFormItem item={item} />)}</div>
             <div className="flex justify-center gap-5">{formItems1.map(item => <RenderFormItem item={item} />)}</div>
+            <Form.Item
+                className="w-full"
+                label={typeFormItem.label}
+                name={typeFormItem.name}
+                rules={typeFormItem.rules}
+                key={typeFormItem.name}
+            >
+                {typeFormItem.component}
+            </Form.Item>
             <Form.Item>
                 <div className="w-full justify-between flex">
                     <Button onClick={() => setPlan({})} className="btn btn-secondary">Cancel</Button>
                     <Button type="primary" htmlType="submit" className="btn btn-primary">Submit</Button>
                 </div>
             </Form.Item>
-        </Form>
+        </Form >
     )
 }
 
