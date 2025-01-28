b238fbd49e5f0d629e3fc0d1aed57903374848d0
RamaSai2519 <rchandik@gitam.in>
2025-01-28 11:13:57 +0530

    feat: Enhance axiosHelper to display environment notifications and set FINAL_URL accordingly

--- src/services/axiosHelper.js
+++ src/services/axiosHelper.js
@@ -1,12 +1,22 @@
 import axios from "axios";
+import { message } from "antd";
 
 const PROD_URL = 'https://6x4j0qxbmk.execute-api.ap-south-1.amazonaws.com/main';
 const DEV_URL = 'https://wivq47mgh6.execute-api.ap-south-1.amazonaws.com/dev';
 const LOCAL_URL = 'http://localhost:8080';
 const ENV = process.env.REACT_APP_ENV;
 
-const FINAL_URL = ENV === 'dev' ? DEV_URL : ENV === 'local' ? LOCAL_URL : PROD_URL;
-console.log("🚀 ~ FINAL_URL:", FINAL_URL)
+let FINAL_URL = '';
+if (ENV === 'dev') {
+  FINAL_URL = DEV_URL
+  message.info('You are in Development Environment');
+} else if (ENV === 'local') {
+  FINAL_URL = LOCAL_URL
+  message.info('You are in Local Environment');
+} else {
+  FINAL_URL = PROD_URL
+}
+
 export const MARK_URL = "https://mark.sukoonunlimited.com";
 
 export const Raxios = axios.create({ baseURL: FINAL_URL });
