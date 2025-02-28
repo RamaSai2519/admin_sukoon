d7da1e34be162041102bcd63ff1ef850a4ae9e14
RamaSai2519 <rchandik@gitam.in>
2025-02-28 11:51:17 +0530

    feat: Update app version to 6.7.2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import CallList from './CallList/CallList';
 import EventDetails from './EventDetails';
 
 const App = () => {
-  const appVersion = '6.6.8';
+  const appVersion = '6.7.2';
   const { darkMode } = useDarkMode();
   const [isLoggedIn, setIsLoggedIn] = useState(
     localStorage.getItem('isLoggedIn') === 'true'
