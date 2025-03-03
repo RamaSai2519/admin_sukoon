5a298ce5a653d831503149961dae12447a9cad58
RamaSai2519 <rchandik@gitam.in>
2025-03-03 11:57:31 +0530

    feat: Refactor DashboardTab component by removing countdown functionality and related state management

--- src/AdminDashboard/DashboardTabs/DashboardTab.js
+++ src/AdminDashboard/DashboardTabs/DashboardTab.js
@@ -1,61 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useStats } from '../../contexts/useData';
 import LazyLoad from '../../components/LazyLoad/lazyload';
 import DashboardTile from '../../components/DashboardTile';
 import DayGraph from '../../components/DaysGraph/DaysGraph';
 import CallGraph from '../../components/CallGraph/CallGraph';
-import { Progress, Modal, InputNumber, Checkbox } from 'antd';
 import LatestCallsTable from '../../components/LatestCallsTable';
 import ExpertGraph from '../../components/ExpertGraph/ExpertGraph';
 import HourCallChart from '../../components/HourCallChart/HourCallChart';
 import OnlineSaarthisTable from '../../components/OnlineSaarthisTable/OnlineSaarthisTable';
 
 const DashboardTab = () => {
-  const [countdownEnabled, setCountdownEnabled] = useState(() => localStorage.getItem('countdownEnabled') === 'true');
-  const [countdown, setCountdown] = useState(() => parseInt(localStorage.getItem('countdown') || '30'));
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [newCountdown, setNewCountdown] = useState(countdown);
   const { stats } = useStats();
-
-  useEffect(() => {
-    if (countdownEnabled) {
-      const interval = setInterval(() => {
-        setCountdown(prevCountdown => prevCountdown > 0 ? prevCountdown - 1 : newCountdown);
-        if (countdown === 0) {
-          window.location.reload();
-        }
-      }, 1000);
-      return () => clearInterval(interval);
-    }
-  }, [countdown, newCountdown, countdownEnabled]);
-
-  const handleCircleClick = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    localStorage.setItem('countdown', newCountdown);
-    localStorage.setItem('countdownEnabled', countdownEnabled);
-    setCountdown(newCountdown);
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
 
   return (
     <LazyLoad>
       <div id='dashboardTab' className='min-h-screen'>
-        <div className='fixed top-4 right-4 z-50' onClick={handleCircleClick}>
-          <Progress type="circle" percent={(countdown / newCountdown) * 100} format={() => countdown} size={30} />
-        </div>
-        <Modal title="Set Countdown Timer" open={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-          <div className='flex flex-col gap-2'>
-            <InputNumber min={1} value={newCountdown} onChange={setNewCountdown} />
-            <Checkbox checked={countdownEnabled} onChange={e => setCountdownEnabled(e.target.checked)}>Countdown</Checkbox>
-          </div>
-        </Modal>
         <div id='parent-grid' className='flex flex-col w-full h-full'>
           <div id='stats-grid' className='grid grid-cols-5'>
             <DashboardTile title="Total Calls">
